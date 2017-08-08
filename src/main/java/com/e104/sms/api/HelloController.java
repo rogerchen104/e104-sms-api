34efb626b4d95c677760a1c3ac3c0fbb0d342542
roger.chen <roger.chen@104.com.tw>
2017-08-08 15:38:47 +0800

    first commit

---
+++ src/main/java/com/e104/sms/api/HelloController.java
@@ -0,0 +1,11 @@
+package com.e104.sms.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+    @RequestMapping("/table/list")
+    public void getTableList() {
+    		System.out.println("Hello!!!!!!!!");
+    }
+}
